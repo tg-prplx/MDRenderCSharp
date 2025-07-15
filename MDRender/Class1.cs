6a4bc07d0fd444052074f1aadb22def212a2a0b2
prplx <whytomatic@gmail.com>
2025-07-15 21:03:17 +0100

    Добавьте файлы проекта.

---
+++ MDRender/Class1.cs
@@ -0,0 +1,198 @@
+﻿using System.Text;
+
+namespace MDRender
+{
+    public class MRender
+    {
+        StringBuilder sb;
+        private string InputText;
+        private HashSet<string> EnabledStyles = new HashSet<string>();
+        private static readonly char[] TrigerSymbols = { '`', '#', '*', '_', '>', '\n', '~', '\\' };
+        private static readonly string[] HeaderStyles = { TextStyles.Header1, TextStyles.Header2, TextStyles.Header3 };
+
+        private char _GetNextChar(ref int index)
+        {
+            if (index + 1 < InputText.Length)
+                return InputText[++index];
+            return '\0';
+        }
+
+        private void _SVChar(char c, ref int Index)
+        {
+            int startIdx = Index;
+            switch (c)
+            {
+                case '`':
+                    if (_GetNextChar(ref Index) == '`' && _GetNextChar(ref Index) == '`') // code block
+                    {
+                        if (!EnabledStyles.Contains(TextStyles.CodeBlock))
+                        {
+                            sb.Append(TextStyles.CodeBlock);
+                            EnabledStyles.Add(TextStyles.CodeBlock);
+                        }
+                        else
+                        {
+                            sb.Append(TextStyles.ResetCodeBlock);
+                            EnabledStyles.Remove(TextStyles.CodeBlock);
+                        }
+                    }
+                    else // inline code
+                    {
+                        Index = startIdx;
+                        if (!EnabledStyles.Contains(TextStyles.InlineCode))
+                        {
+                            sb.Append(TextStyles.InlineCode);
+                            EnabledStyles.Add(TextStyles.InlineCode);
+                        }
+                        else
+                        {
+                            sb.Append(TextStyles.ResetInlineCode);
+                            EnabledStyles.Remove(TextStyles.InlineCode);
+                        }
+                    }
+                    break;
+
+                case '*':
+                    if (_GetNextChar(ref Index) == '*') // bold
+                    {
+                        if (!EnabledStyles.Contains(TextStyles.Bold))
+                        {
+                            sb.Append(TextStyles.Bold);
+                            EnabledStyles.Add(TextStyles.Bold);
+                        }
+                        else
+                        {
+                            sb.Append(TextStyles.ResetBold);
+                            EnabledStyles.Remove(TextStyles.Bold);
+                        }
+                    }
+                    else // italic
+                    {
+                        Index = startIdx;
+                        if (!EnabledStyles.Contains(TextStyles.Cursive))
+                        {
+                            sb.Append(TextStyles.Cursive);
+                            EnabledStyles.Add(TextStyles.Cursive);
+                        }
+                        else
+                        {
+                            sb.Append(TextStyles.ResetCursive);
+                            EnabledStyles.Remove(TextStyles.Cursive);
+                        }
+                    }
+                    break;
+
+                case '\\': // escaping
+                    if (Index + 1 < InputText.Length)
+                    {
+                        sb.Append(InputText[Index + 1]);
+                        Index++;
+                    }
+                    break;
+
+                case '\n':
+                    if (EnabledStyles.Overlaps(HeaderStyles))
+                    {
+                        sb.Append(TextStyles.ResetHeader);
+                        foreach (var h in HeaderStyles) EnabledStyles.Remove(h);
+                    }
+                    else if (EnabledStyles.Contains(TextStyles.QuoteStyle))
+                    {
+                        sb.Append(TextStyles.ResetQuote);
+                        EnabledStyles.Remove(TextStyles.QuoteStyle);
+                    }
+                    sb.Append('\n');
+                    break;
+
+                case '>':
+                    if (!EnabledStyles.Contains(TextStyles.QuoteStyle))
+                    {
+                        sb.Append(TextStyles.QuoteStyle);
+                        EnabledStyles.Add(TextStyles.QuoteStyle);
+                    }
+                    else
+                    {
+                        sb.Append(TextStyles.ResetQuote);
+                        EnabledStyles.Remove(TextStyles.QuoteStyle);
+                    }
+                    break;
+
+                case '~':
+                    if (_GetNextChar(ref Index) == '~')
+                    {
+                        if (!EnabledStyles.Contains(TextStyles.Crossed))
+                        {
+                            sb.Append(TextStyles.Crossed);
+                            EnabledStyles.Add(TextStyles.Crossed);
+                        }
+                        else
+                        {
+                            sb.Append(TextStyles.ResetCrossed);
+                            EnabledStyles.Remove(TextStyles.Crossed);
+                        }
+                    }
+                    else
+                    {
+                        Index = startIdx;
+                        sb.Append('~');
+                    }
+                    break;
+                case '#':
+                    int start = Index;
+                    int count = 1;
+
+                    while (_GetNextChar(ref Index) == '#')
+                        count++;
+
+                    if (Index + 1 < InputText.Length && InputText[Index + 1] == ' ')
+                        Index++;
+
+                    switch (count)
+                    {
+                        case 1:
+                            sb.Append(TextStyles.Header1);
+                            EnabledStyles.Add(TextStyles.Header1);
+                            break;
+                        case 2:
+                            sb.Append(TextStyles.Header2);
+                            EnabledStyles.Add(TextStyles.Header2);
+                            break;
+                        case 3:
+                            sb.Append(TextStyles.Header3);
+                            EnabledStyles.Add(TextStyles.Header3);
+                            break;
+                        default:
+                            Index = start;
+                            sb.Append('#');
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        public string GetMDTerminalText(string Text)
+        {
+            sb = new StringBuilder();
+            InputText = Text ?? "";
+            EnabledStyles.Clear();
+
+            for (int i = 0; i < InputText.Length; i++)
+            {
+                char ch = InputText[i];
+                if (TrigerSymbols.Contains(ch))
+                {
+                    _SVChar(ch, ref i);
+                }
+                else
+                {
+                    sb.Append(ch);
+                }
+            }
+
+            if (EnabledStyles.Count > 0)
+                sb.Append("\x1b[0m");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ MDRender/Styles.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDRender
+{
+    internal static class TextStyles
+    {
+        public const string Bold = "\x1b[1m";
+        public const string ResetBold = "\x1b[22m";
+
+        public const string Cursive = "\x1b[3m";
+        public const string ResetCursive = "\x1b[23m";
+
+        public const string Underline = "\x1b[4m";
+        public const string ResetUnderline = "\x1b[24m";
+
+        public const string Crossed = "\x1b[9m";
+        public const string ResetCrossed = "\x1b[29m";
+
+        public const string InlineCode = "\x1b[48;5;240m\x1b[38;5;15m";
+        public const string ResetInlineCode = "\x1b[0m";
+
+        public const string QuoteStyle = "\x1b[90m";
+        public const string ResetQuote = "\x1b[0m";
+
+        public const string Header1 = "\x1b[1;34m";
+        public const string Header2 = "\x1b[1;32m";
+        public const string Header3 = "\x1b[1;36m";
+
+        public const string ResetHeader = "\x1b[0m";
+
+        public const string CodeBlock = "\x1b[48;5;240m\x1b[30m";
+        public const string ResetCodeBlock = "\x1b[0m";
+    }
+}
